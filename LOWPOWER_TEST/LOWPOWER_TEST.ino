abd178446d215fbb0130412e293b0c0b1ff80e5c
robertomc2007 <robertomc2007@gmail.com>
2016-11-10 17:12:07 -0300

    Add files via upload

---
+++ LOWPOWER_TEST/LOWPOWER_TEST.ino
@@ -0,0 +1,27 @@
+// **** INCLUDES *****
+#include "LowPower.h"
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.println(F("INICIO LOWPOWER TEST"));
+}
+
+void loop()
+{
+  Serial.println(F("ANTES DE POWERDOWN"));
+  Serial.flush();
+  // Enter power down state for 8 s with ADC and BOD module disabled
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  delay(1);
+  Serial.println(F("ANTES DE IDLE"));
+  Serial.flush();
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
+                SPI_OFF, USART0_OFF, TWI_OFF);
+  delay(1);
+  Serial.println(F("DESPUES DE IDLE"));
+  Serial.flush();
+
+
+}

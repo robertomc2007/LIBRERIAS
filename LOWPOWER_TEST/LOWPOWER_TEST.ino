98560a54b0bbb1cd1a2507c5be71c8ed405b8a0c
robertomc2007 <robertomc2007@gmail.com>
2016-11-14 16:59:05 -0300

    Update LOWPOWER_TEST.ino

--- LOWPOWER_TEST/LOWPOWER_TEST.ino
+++ LOWPOWER_TEST/LOWPOWER_TEST.ino
@@ -6,22 +6,31 @@ void setup()
   Serial.begin(115200);
 
   Serial.println(F("INICIO LOWPOWER TEST"));
+  
+  pinMode(13,OUTPUT);
+  
 }
 
 void loop()
 {
-  Serial.println(F("ANTES DE POWERDOWN"));
+  
+  Serial.println(F("PLENO CONSUMO POR 4 SEGUNDOS"));
   Serial.flush();
-  // Enter power down state for 8 s with ADC and BOD module disabled
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  delay(1);
-  Serial.println(F("ANTES DE IDLE"));
+  
+  digitalWrite(13,HIGH);
+  
+  delay(4000);
+
+  Serial.println(F("ANTES DE REDUCCION DE ENERGÍA"));
   Serial.flush();
+  
+  digitalWrite(13,LOW);
+  
   LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
                 SPI_OFF, USART0_OFF, TWI_OFF);
-  delay(1);
-  Serial.println(F("DESPUES DE IDLE"));
-  Serial.flush();
-
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
+                SPI_OFF, USART0_OFF, TWI_OFF);
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
+                SPI_OFF, USART0_OFF, TWI_OFF);
 
 }
